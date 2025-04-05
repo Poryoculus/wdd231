795c9c5e02ded23a277b46e595ea44195e90f904
Poryoculus <rodrigopachecojor@gmail.com>
2025-04-04 23:20:47 -0300

    the footer script in the thanksyou-html

--- chamber/scripts/thanksyou.js
+++ chamber/scripts/thanksyou.js
@@ -1,6 +1,31 @@
 const forminfo = new URLSearchParams(window.location.search);
 const form = document.querySelector(".form-information");
+
+// Footer script to display current year
+const year = document.querySelector("#currentYear");
 const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+// Display last modification date
+const lastModified = document.querySelector("#lastModification");
+lastModified.textContent = `${new Date(document.lastModified)}`;
+
+// Hamburger menu script for responsive navigation
+function myFunction() {
+  var x = document.querySelector(".navigation");
+  x.style.display = x.style.display === "block" ? "none" : "block";
+}
+
+// Adjust navigation display on window resize
+window.addEventListener("resize", function () {
+  var x = document.querySelector(".navigation");
+  x.style.display = window.innerWidth > 600 ? "flex" : "none";
+});
+
+
+
+
+
 
 const timestamp = forminfo.get("timestamp") || today.getTime();
 form.innerHTML = `
