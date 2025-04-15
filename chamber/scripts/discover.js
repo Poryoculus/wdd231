6389b996eb16f4f8c69427fe4549189d5d577873
Poryoculus <rodrigopachecojor@gmail.com>
2025-04-15 04:13:21 -0400

    lighthouse problems

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -24,6 +24,12 @@ const displayCards = (Cards) => {
     const portrait = document.createElement("img");
     portrait.setAttribute("src", business.location);
     portrait.setAttribute("alt", business.name);
+    if (Object.values(Cards).indexOf(business) > 1) {
+      portrait.setAttribute("loading", "lazy");
+    } else {
+      portrait.setAttribute("fetchpriority", "high");
+      portrait.setAttribute("rel", "preload");
+    }
     portrait.classList.add("portrait");
 
     const description = document.createElement("p");
@@ -88,3 +94,15 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 // Display last modification date
 const lastModified = document.querySelector("#lastModification");
 lastModified.textContent = `${new Date(document.lastModified)}`;
+
+// Hamburger menu script for responsive navigation
+function myFunction() {
+  var x = document.querySelector(".navigation");
+  x.style.display = x.style.display === "block" ? "none" : "block";
+}
+
+// Adjust navigation display on window resize
+window.addEventListener("resize", function () {
+  var x = document.querySelector(".navigation");
+  x.style.display = window.innerWidth > 600 ? "flex" : "none";
+});
